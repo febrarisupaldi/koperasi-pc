93a1b803faf9741cb5d94f4cb302559d982e7663
Billal Fauzan <billal.xcode@gmail.com>
2023-09-24 00:20:54 +0700

    :sparkles: authentication feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,20 +6,21 @@
         <div class="col-lg-5 col-12">
             <div id="auth-left">
                 <div class="auth-logo">
-                    <a href="index.html"><img src="assets/static/images/logo/logo.svg" alt="Logo"></a>
+                    <a href="index.html"><img src="{{ asset('assets/static/images/logo/logo.svg') }}" alt="Logo"></a>
                 </div>
                 <h1 class="auth-title">Log in.</h1>
                 <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
 
-                <form action="index.html">
+                <form action="{{ route('auth.login.store') }}" method="POST">
+                    @csrf
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="text" class="form-control form-control-xl" placeholder="Username">
+                        <input type="text" class="form-control form-control-xl" placeholder="Username" name="username">
                         <div class="form-control-icon">
                             <i class="bi bi-person"></i>
                         </div>
                     </div>
                     <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="password" class="form-control form-control-xl" placeholder="Password">
+                        <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
                         <div class="form-control-icon">
                             <i class="bi bi-shield-lock"></i>
                         </div>
