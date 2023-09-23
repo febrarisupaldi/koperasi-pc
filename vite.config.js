9ea480e04eb19290d8aef6dc4951805d43bcac72
Billal Fauzan <billal.xcode@gmail.com>
2023-09-23 23:07:28 +0700

    :sparkles: introduce new project

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import laravel from "laravel-vite-plugin";
+import { resolve } from 'path';
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            "~bootstrap": resolve(__dirname, "node_modules/bootstrap"),
+            "~bootstrap-icons": resolve(
+                __dirname,
+                "node_modules/bootstrap-icons",
+            ),
+            "~perfect-scrollbar": resolve(
+                __dirname,
+                "node_modules/perfect-scrollbar",
+            ),
+            "~@fontsource": resolve(__dirname, "node_modules/@fontsource"),
+        },
+    },
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+});
