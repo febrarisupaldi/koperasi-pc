e4fbba20931503a08daedefbf472716a599e82bf
Billal Fauzan <billal.xcode@gmail.com>
2023-09-24 18:35:48 +0700

    :truck: move folder js in public to resources js

--- resources/js/mazer.js
+++ resources/js/mazer.js
@@ -4,5 +4,4 @@ import bootstrapBundle from "bootstrap/dist/js/bootstrap.bundle"
 window.bootstrap = bootstrapBundle
 
 // We could import PerfectScrollbar directly in the sidebar module
-import "./../static/js/components/sidebar"
-
+import './components/sidebar'
