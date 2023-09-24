c6facb3f54701fda13012f01940cc39788c031db
Billal Fauzan <billal.xcode@gmail.com>
2023-09-24 19:00:25 +0700

    :bug: remove semicolon in pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - {{ config('app.name') }}</title>
-    @vite(['resources/sass/app.scss', 'resources/sass/themes/dark/app-dark.scss']);
+    @vite(['resources/sass/app.scss', 'resources/sass/themes/dark/app-dark.scss'])
     <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.svg') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('assets/static/images/logo/favicon.png') }}" type="image/png">
 </head>
